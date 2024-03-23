03e8c9964cdfc086d38d0129a0e8bd691a6bf580
Md. Shamim Hossain <shamim.cse.gub@gmail.com>
2024-03-23 15:58:38 +0600

    wip:blog update

--- resources/views/frontend/blog.blade.php
+++ resources/views/frontend/blog.blade.php
@@ -18,9 +18,9 @@
                             <h4 class="blog-title">{{ $blog->title }}</h4>
                             <p class="card-text"><small class="text-muted">{{ \Carbon\Carbon::parse($blog->created_at)->format('d M Y') }}</small></p>
                             <p class="blog-text p-0 m-0">{!! excerpt($blog->details, 450) !!}</p>
-                            <button type="button" class="btn btn-dark rounded-0 mt-3 d-flex justify-content-center align-items-center">
+                            <button type="button" class="btn border-0 rounded-0 mt-3 d-flex justify-content-center align-items-center" style="background: #087194">
                                 <a class="text-decoration-none text-light" href="{{ route('blog-details', $blog->slug) }}" role="button">Read More</a>
-                                <i class="fa-solid fa-caret-right mt-1 ms-2"></i>
+                                <i class="fa-solid fa-caret-right mt-1 ms-2 text-white"></i>
                             </button>
                         </div>
                     </div>
